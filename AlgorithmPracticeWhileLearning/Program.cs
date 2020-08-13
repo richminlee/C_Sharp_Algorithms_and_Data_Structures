feae241918a1196ebcc432969ebc33cbaba8bacb
Richard Lee <lee.richardm@gmail.com>
2020-08-13 08:00:15 -0700

    starting Algos again!

---
+++ AlgorithmPracticeWhileLearning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgorithmPracticeWhileLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
