5430d845aacf923b278e96343f0c239eeefab01b
Richard Lee <lee.richardm@gmail.com>
2020-08-15 08:17:25 -0700

    Reversed Int

---
+++ ReversedInt/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ReversedInt
+{
+    class Program
+    {
+        static int reverseInt(int inputInt)
+        {
+            int reversedInt = 0;
+            while (inputInt > 0)
+            {
+                if (reversedInt != 0) reversedInt *= 10;
+                reversedInt += inputInt % 10;
+                inputInt /= 10;
+            }
+            return reversedInt;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(reverseInt(1234));
+        }
+    }
+}
