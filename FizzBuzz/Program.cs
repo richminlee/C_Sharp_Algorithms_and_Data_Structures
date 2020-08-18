dcb5867a8a22de0466b227bb538616a6059693c8
Richard Lee <lee.richardm@gmail.com>
2020-08-17 22:09:17 -0700

    After FizzBuzz(Classic!)

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        public static List<string> FizzBuzz(int n)
+        {
+            var resultString = new List<string>();
+            for(int i = 1; i<=n; i++ )
+            {
+                if(i % 3 == 0 && i % 5 == 0)
+                {
+                    resultString.Add("FizzBuzz");
+                }                
+                else if(i % 3 == 0)
+                {
+                    resultString.Add("Fizz");
+                }                
+                else if(i % 5 == 0)
+                {
+                    resultString.Add("Buzz");
+                }                
+                else
+                {
+                    resultString.Add($"{i}");
+                }
+            }
+            return resultString;
+        }
+        static void Main(string[] args)
+        {
+            var result = FizzBuzz(15);
+            foreach(var key in result)
+            {
+                Console.WriteLine(key);
+            }
+        }
+    }
+}
--- ReversedInt/Program.cs
+++ ReversedInt/Program.cs
@@ -7,17 +7,19 @@ namespace ReversedInt
         static int reverseInt(int inputInt)
         {
             int reversedInt = 0;
-            while (inputInt > 0)
+            while (inputInt != 0)
             {
                 if (reversedInt != 0) reversedInt *= 10;
                 reversedInt += inputInt % 10;
                 inputInt /= 10;
             }
+            if (reversedInt < -2147483648 && reversedInt > 2147483647) return 0;
+
             return reversedInt;
         }
         static void Main(string[] args)
         {
-            Console.WriteLine(reverseInt(1234));
+            Console.WriteLine(reverseInt(2147483647));
         }
     }
 }
