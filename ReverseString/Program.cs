9267626558ce56a94377cc06824710c30cce53fd
Richard Lee <lee.richardm@gmail.com>
2020-08-18 22:21:21 -0700

    reversedString

---
+++ ReverseString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        public static char[] ReverseString(char[] s)
+        {
+            int left = 0;
+            int right = s.Length - 1;
+            char temp;
+            while (left < right)
+            {
+                temp = s[left];
+                s[left] = s[right];
+                s[right] = temp;
+                left++;
+                right--;
+            }
+            return s;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseString(new char[] { 'h', 'e', 'l', 'l', 'o' })); 
+        }
+    }
+}
