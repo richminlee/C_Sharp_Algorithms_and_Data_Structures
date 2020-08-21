ff08d091a1010f166fc38a1cd55e8ed145da1add
Richard Lee <lee.richardm@gmail.com>
2020-08-20 20:45:03 -0700

    VowelFromString

---
+++ LongestString/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LongestString
+{
+    class Program
+    {
+        //public static string LongestString(string input)
+        //{
+        //    string result = "";
+        //    string[] allStrings = input.Split(' ');
+        //    for(int i = 0; i< allStrings.Length; i++)
+        //    {
+        //        if(result.Length < allStrings[i].Length)
+        //        {
+        //            result = allStrings[i];
+        //        }
+        //    }
+        //    return result;
+        //}
+        public static int LongestString(string input)
+        {
+            string[] allStrings = input.Split(' ');
+            int result = allStrings.Length;
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LongestString("I am lost"));
+        }
+    }
+}
---
+++ VowelsFromString/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VowelsFromString
+{
+    class Program
+    {
+        public static int VowelsFromString(string input)
+        {
+            string lowerCaseInput = input.ToLower();
+            int result = 0;
+            int yResult = 0;
+            var VowelsDict = new Dictionary<char, int>();
+            VowelsDict.Add('a', 1);
+            VowelsDict.Add('e', 1);
+            VowelsDict.Add('i', 1);
+            VowelsDict.Add('o', 1);
+            VowelsDict.Add('u', 1);
+            for(int i = 0; i < input.Length; i++)
+            {
+                if (VowelsDict.ContainsKey(lowerCaseInput[i])) result++;
+                if (lowerCaseInput[i] == 'y') yResult++;
+            }
+            if (result == 0) result = yResult;
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(VowelsFromString("you"));
+        }
+    }
+}
