0b44abf74fcf8036db217f5ab77748c355de5eee
Richard Lee <lee.richardm@gmail.com>
2020-08-24 22:28:27 -0700

    started Length of longest substring

---
+++ LengthOfLongestSubstring/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LengthOfLongestSubstring
+{
+    class Program
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            int result = 0;
+            var hashMap = new Dictionary<string, int>();
+            for(int i = 0; i< s.Length; i++)
+            {
+                if (!(hashMap[s[i]]))
+                {
+
+                }
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
