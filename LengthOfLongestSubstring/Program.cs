57c2da65df5d0fc45ff0e6f748533e8451d74aed
Richard Lee <lee.richardm@gmail.com>
2020-08-26 10:49:13 -0700

    length of longest substring

--- LengthOfLongestSubstring/Program.cs
+++ LengthOfLongestSubstring/Program.cs
@@ -5,22 +5,43 @@ namespace LengthOfLongestSubstring
 {
     class Program
     {
-        public int LengthOfLongestSubstring(string s)
+        public static int LengthOfLongestSubstring(string s)
         {
-            int result = 0;
-            var hashMap = new Dictionary<string, int>();
+            int resultidx = 0;
+            int loopIdx = 0;
+            var resultList = new List<int>();
+            resultList.Add(0);
+            var hashMap = new Dictionary<char, int>();
             for(int i = 0; i< s.Length; i++)
             {
-                if (!(hashMap[s[i]]))
+                if (hashMap.ContainsKey(s[i]))
                 {
-
+                    loopIdx++;
+                    i = loopIdx;
+                    hashMap.Clear();
+                    hashMap.Add(s[i], 1);
+                    resultList.Add(1);
+                    resultidx++;
+                }
+                else
+                {
+                    hashMap.Add(s[i], 1);
+                    resultList[resultidx]++;
+                }
+            }
+            int result = resultList[0];
+            for (int i = 0; i < resultList.Count; i++)
+            {
+                if(resultList[i] > result)
+                {
+                    result = resultList[i];
                 }
             }
             return result;
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(LengthOfLongestSubstring("dvdf"));
         }
     }
 }
