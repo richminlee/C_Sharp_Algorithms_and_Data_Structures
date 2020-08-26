8d76fe5d2d16ba3e385897b6267d8def0929f3a1
Richard Lee <lee.richardm@gmail.com>
2020-08-26 15:01:34 -0700

    Frequency Counter

---
+++ FrequencyCounter/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FrequencyCounter
+{
+    class Program
+    {
+        public static bool Same(int[] arr1, int[] arr2)
+        {
+            if(arr1.Length != arr2.Length)
+            {
+                return false;
+            }
+            var frequencyCounter1 = new Dictionary<int, int>();
+            var frequencyCounter2 = new Dictionary<int, int>();
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if(!frequencyCounter1.ContainsKey(arr1[i]))
+                {
+                    frequencyCounter1.Add(arr1[i], 1);
+                }
+                else
+                {
+                    frequencyCounter1[arr1[i]]++;
+                }
+            }
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                if (!frequencyCounter2.ContainsKey(arr2[i]))
+                {
+                    frequencyCounter2.Add(arr2[i], 1);
+                }
+                else
+                {
+                    frequencyCounter2[arr2[i]]++;
+                }
+            }
+            foreach(var key in frequencyCounter1)
+            {
+                if (!frequencyCounter2.ContainsKey(key.Key * key.Key))
+                {
+                    return false;
+                }
+                if(frequencyCounter2[key.Key * key.Key] != frequencyCounter1[key.Key])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Same(new int[] { 1, 2, 5, 3, 2 }, new int[] { 9, 1, 25, 4, 4 }));
+        }
+    }
+}
