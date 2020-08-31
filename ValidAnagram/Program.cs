7e49cd59810bf5fa6b650aeaf370d2297077fcf0
Richard Lee <lee.richardm@gmail.com>
2020-08-31 15:59:02 -0700

    refactored valid anagram

--- ValidAnagram/Program.cs
+++ ValidAnagram/Program.cs
@@ -7,8 +7,9 @@ namespace ValidAnagram
     {
         public static bool ValidAnagram(string input1, string input2)
         {
+            if (input1.Length != input2.Length) return false;
+
             var string1Dictionary = new Dictionary<char, int>();
-            var string2Dictionary = new Dictionary<char, int>();
             for (int i = 0; i < input1.Length; i++)
             {
                 
@@ -20,32 +21,15 @@ namespace ValidAnagram
                 {
                     string1Dictionary.Add(input1[i], 1);
                 }
-            }            
+            }
             for (int i = 0; i < input2.Length; i++)
             {
-                if (string2Dictionary.ContainsKey(input2[i]))
-                {
-                    string2Dictionary[input2[i]]++;
-                }
-                else
-                {
-                    string2Dictionary.Add(input2[i], 1);
-                }
-            }
-            foreach(var num in string1Dictionary)
-            {
-                if (!string2Dictionary.ContainsKey(num.Key) || string2Dictionary[num.Key] != num.Value)
+                if (!string1Dictionary.ContainsKey(input2[i]) || string1Dictionary[input2[i]] == 0)
                 {
                     return false;
                 }
-            }            
-            foreach(var num in string2Dictionary)
-            {
-                if (!string1Dictionary.ContainsKey(num.Key) || string1Dictionary[num.Key] != num.Value)
-                {
-                    return false;
-                }
-            }
+                string1Dictionary[input2[i]]--;
+            }           
             return true;
         }
         static void Main(string[] args)
