f59f1d795fd2b8db77838cab3ea1f77ffb206a3e
Richard Lee <lee.richardm@gmail.com>
2020-08-31 20:59:51 -0700

    completed Max sub array sum

---
+++ MaxSubArraySum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO.MemoryMappedFiles;
+
+namespace MaxSubArraySum
+{
+    class Program
+    {
+        public static int MaxSubArraySum(int[] input, int number)
+        {
+            int temp = 0;
+            int left = 0;
+            int right = 0;
+            while (right != number)
+            {
+                temp += input[right];
+                right++;
+            }
+            int result = temp;
+            while(right != input.Length)
+            {
+                temp += input[right];
+                temp -= input[left];
+                if(result < temp)
+                {
+                    result = temp;
+                }
+                right++;
+                left++;
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxSubArraySum(new int[] { 999,2,1,6,1}, 4 ));
+        }
+    }
+}
