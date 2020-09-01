eb6864d8ad609583a6ba81f222e587940c2d74b7
Richard Lee <lee.richardm@gmail.com>
2020-08-31 20:28:28 -0700

    refactored count unique values

--- CountUniqueValues/Program.cs
+++ CountUniqueValues/Program.cs
@@ -6,7 +6,6 @@ namespace CountUniqueValues
     {
         public static int CountUniqueValues(int[] input)
         {
-            int result = 1;
             int left = 0;
             int right = 1;
             while(right != input.Length)
@@ -17,17 +16,17 @@ namespace CountUniqueValues
                 }
                 else
                 {
-                    result++;
-                    left = right;
+                    left++;
+                    input[left] = input[right];
                     right++;
                 }
 
             }
-            return result;
+            return left+1;
         }
         static void Main(string[] args)
         {
-            Console.WriteLine(CountUniqueValues(new int[] {-2,-1,-1,0,1 }));
+            Console.WriteLine(CountUniqueValues(new int[] {1,2,3,4,4,4,7,7,12,12,13}));
         }
     }
 }
