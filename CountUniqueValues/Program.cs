c27dc3292fdc40b8c5f64c3cd9e869c1ae3d19b4
Richard Lee <lee.richardm@gmail.com>
2020-08-31 19:26:30 -0700

    Completed Count Unique Values

---
+++ CountUniqueValues/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CountUniqueValues
+{
+    class Program
+    {
+        public static int CountUniqueValues(int[] input)
+        {
+            int result = 1;
+            int left = 0;
+            int right = 1;
+            while(right != input.Length)
+            {
+                if(input[left] == input[right])
+                {
+                    right++;
+                }
+                else
+                {
+                    result++;
+                    left = right;
+                    right++;
+                }
+
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountUniqueValues(new int[] {-2,-1,-1,0,1 }));
+        }
+    }
+}
