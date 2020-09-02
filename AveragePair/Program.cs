37c9fa4d129a68d4fd27c07112e40b25d2bc6f9d
Richard Lee <lee.richardm@gmail.com>
2020-09-02 12:13:09 -0700

    Completed Average Pair

---
+++ AveragePair/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace AveragePair
+{
+    class Program
+    {
+        public static bool AveragePair(int[] input, double target)
+        {
+            int left = 0;
+            int right = input.Length - 1;
+            while (left < right)
+            {
+                if ((double)(input[left]) + (double)(input[right]) / 2 == target) return true;
+                else if ((double)(input[left]) + (double)(input[right]) / 2 < target) left++;
+                else right--;
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AveragePair(new int[] { 1,3,3,5,6,7,10,12,19}, 8));
+        }
+    }
+}
