e0ad78be97ad5ab1bfdb55a482b2f9ebc1205453
Richard Lee <lee.richardm@gmail.com>
2020-09-22 14:43:46 -0700

    added defanging

---
+++ DefangingIP/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace DefangingIP
+{
+    class Program
+    {
+        public static string DefangIPaddr(string address)
+        {
+            string[] splitString = address.Split(".");
+            string result = String.Join("[.]", splitString);
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DefangIPaddr("192.168.254.2"));
+        }
+    }
+}
