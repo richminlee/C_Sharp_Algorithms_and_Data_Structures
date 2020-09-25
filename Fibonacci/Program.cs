b4e36891a3ce24d39b2fd7a8e02b53bbd4b3204f
Richard Lee <lee.richardm@gmail.com>
2020-09-24 21:34:38 -0700

    Fibonacci Number

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        public static int Fib(int N)
+        {
+            if (N == 1) return 1;
+            if (N == 0) return 0;
+            return Fib(N - 1) + Fib(N - 2);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fib(12));
+        }
+    }
+}
