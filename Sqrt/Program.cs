391636989ec96ad6c6f52b55615301a4999568ed
Richard Lee <lee.richardm@gmail.com>
2020-10-04 22:46:55 -0700

    Added SQRT

---
+++ Sqrt/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Sqrt
+{
+    class Program
+    {
+        public static int MySqrt(int x)
+        {
+            int result = (int)Math.Pow(x, 0.5);
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MySqrt(4));
+        }
+    }
+}
