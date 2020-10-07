eceadab00eb61b76fa87e7ce2e529172a472e1ca
Richard Lee <lee.richardm@gmail.com>
2020-10-06 20:57:54 -0700

    Added Reverse words

---
+++ ReverseWords/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseWords
+{
+    class Program
+    {
+        public static string ReverseWords(string s)
+        {
+            string[] splitString = s.Split(" ");
+            for (int i = 0; i < splitString.Length; i++)
+            {
+                string temp = "";
+                for (int j = splitString[i].Length-1; j >= 0; j--)
+                {
+                    temp += splitString[i][j];
+                }
+                splitString[i] = temp;
+            }
+            return String.Join(" ", splitString);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseWords("Let's take LeetCode contest"));
+        }
+    }
+}
---
+++ WhiteboardingDuringClass/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WhiteboardingDuringClass
+{
+    class Program
+    {
+        public static int SumOfEven(int min, int max)
+        {
+            int result = 0;
+            for (int i = min; i <= max; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    result += i;
+                }
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumOfEven(1,10));
+        }
+    }
+}
