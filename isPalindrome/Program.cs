34bb34af01fceb2e7c3bf7767d03ba04d22ba8bf
Richard Lee <lee.richardm@gmail.com>
2020-10-13 21:33:24 -0700

    added IsPalindrome from class

---
+++ isPalindrome/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace isPalindrome
+{
+    class Program
+    {
+        public static bool FindPalindrome(string input)
+        {
+            input = input.ToLower();
+            int i = 0;
+            int j = input.Length - 1;
+            while (j > i)
+            {
+                while (input[i] < 'a' || input[i] > 'z')
+                {
+                    i++;
+                }
+                while (input[j] < 'a' || input[j] > 'z')
+                {
+                    j--;
+                }
+                if (input[i] != input[j])
+                {
+                    return false;
+                }
+                i++;
+                j--;
+            }
+
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindPalindrome("Rac....e ca%r"));
+        }
+    }
+}
