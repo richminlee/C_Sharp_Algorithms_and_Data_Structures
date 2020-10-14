e087e35cda8d3c424c041a04da6912acc170813c
Richard Lee <lee.richardm@gmail.com>
2020-10-14 14:52:22 -0700

    added sockMerchant

---
+++ sockMerchant/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sockMerchant
+{
+    class Program
+    {
+        static int sockMerchant(int n, int[] ar)
+        {
+            Dictionary<int, int> socks = new Dictionary<int, int>();
+            for (int i = 0; i < ar.Length; i++)
+            {
+                if (socks.ContainsKey(ar[i])) socks[ar[i]]++;
+                else
+                {
+                    socks.Add(ar[i], 1);
+                }
+            }
+            int result = 0;
+            foreach (var num in socks.Values)
+            {
+                result += num / 2;
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(sockMerchant(9,new int[] { 10, 20, 20, 10, 10, 30, 50, 10, 20 }));
+        }
+    }
+}
