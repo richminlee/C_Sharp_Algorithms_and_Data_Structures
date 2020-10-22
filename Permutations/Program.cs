0e5c1a86f3335d1638c96f013ed7817dfdf27b44
Richard Lee <lee.richardm@gmail.com>
2020-10-21 18:24:49 -0700

    Added ReverseList

---
+++ Permutations/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Permutations
+{
+    class Program
+    {
+        public List<List<int>> Permute(int[] nums)
+        {
+        
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ReverseList/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ReverseList
+{
+    class Program
+    {
+        public class ListNode {
+            public int val;
+            public ListNode next;
+            public ListNode(int val=0, ListNode next=null) {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public static void DisplayList(ListNode head)
+        {
+            ListNode runner = head;
+            while (runner != null)
+            {
+                Console.WriteLine(runner.val);
+                runner = runner.next;
+            } 
+        }
+        public static ListNode ReverseList(ListNode head)
+        {
+            ListNode pivot = head;
+            ListNode walker = null;
+            while (pivot != null)
+            {
+                ListNode runner = pivot.next;
+                pivot.next = walker;
+                walker = pivot;
+                pivot = runner;
+            }
+            return walker;
+        }
+        static void Main(string[] args)
+        {
+            ListNode five = new ListNode(5);
+            ListNode four = new ListNode(4, five);
+            ListNode three = new ListNode(3, four);
+            ListNode two = new ListNode(2, three);
+            ListNode one = new ListNode(1, two);
+            DisplayList(ReverseList(one));
+        }
+    }
+}
