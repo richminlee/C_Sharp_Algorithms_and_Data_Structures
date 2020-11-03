056d4126a373d94f81673441b4c67fd81c6b0b14
Richard Lee <lee.richardm@gmail.com>
2020-11-02 17:42:05 -0800

    added first recurring character

--- DataStructures/HashTables/Program.cs
+++ DataStructures/HashTables/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HashTables
 {
     class Program
     {
+        static int FirstDuplicate(int[] input)
+        {
+            HashSet<int> result = new HashSet<int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (result.Contains(input[i]))
+                {
+                    return input[i];
+                }
+                else
+                {
+                    result.Add(input[i]);
+                }
+            }
+            return -1;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(FirstDuplicate(new int[] { 2,3,4,5}));
         }
     }
 }
